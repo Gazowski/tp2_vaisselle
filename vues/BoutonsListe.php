1bec6dfe5df9b4b48971c95247902debaa43a138
Gazowski <gael_comeau@hotmail.com>
2019-12-30 08:32:43 -0500

    Vues : - découpage de la vue principale - suppression des vues inutiles - création d'un dossier common

---
+++ vues/BoutonsListe.php
@@ -0,0 +1,4 @@
+
+<button data-js-btn-precedent>Précédent</button>
+<button data-js-btn-suivant>Suivant</button>
+</section>
--- vues/ListeProduits.php
+++ vues/ListeProduits.php
@@ -1,11 +1,9 @@
-<h2>Liste des Produits</h2>
-<ul>
-<?php
-	foreach($data as $produit)
-	{
-?>
-	<li><?= $produit["id"] . " - " . $produit["nom"] . " - prix : " .  $produit["prix"] . "$"?> </li>
-<?php
-	}
-?>
-</ul>
+
+<section data-js-liste-produits>
+	<div data-js-wrapper-liste>
+		<?php foreach($data as $produit) { ?>
+			<article data-js-item><?= $produit["id"] . " - " . $produit["nom"] . " - prix : " .  $produit["prix"] . "$"?> </article>
+		<?php } ?>
+	</div>
+
+
