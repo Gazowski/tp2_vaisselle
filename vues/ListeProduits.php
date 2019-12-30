f2073fddac4037f633920e77665a3679ca1f0947
Gazowski <gael_comeau@hotmail.com>
2019-12-30 11:39:20 -0500

    styles : ajout classe tuile

--- vues/ListeProduits.php
+++ vues/ListeProduits.php
@@ -1,6 +1,6 @@
 <div data-js-wrapper-liste>
 	<?php foreach($data as $produit) { ?>
-		<article data-js-item><?= $produit["id"] . " - " . $produit["nom"] . " - prix : " .  $produit["prix"] . "$"?> </article>
+		<article class="tuile" data-js-item><?= $produit["id"] . " - " . $produit["nom"] . " - prix : " .  $produit["prix"] . "$"?> </article>
 	<?php } ?>
 </div>
 
