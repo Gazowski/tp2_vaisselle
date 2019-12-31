890003d6ff3484ab8ef85573e244812e015aaae0
Gazowski <gael_comeau@hotmail.com>
2019-12-31 13:19:04 -0500

    listeProduits.js : correction sessionstorage le sessionStorage.produitsPanier n'est plus remis a zero au rechargement

--- scripts/ListeProduits.js
+++ scripts/ListeProduits.js
@@ -89,10 +89,15 @@ export class ListeProduits{
         total_panier = parseInt(compteur_panier.innerHTML)
         compteur_panier.innerHTML = total_panier + 1
     }
-
+    
     enregistrer_id_item = (item) =>{
+        //session storage ne prend que les strings
+        //création d'un tableau intérmédiaire qui est transformé en Json avant d'être enregistré en session storage
+        if(sessionStorage.produitsPanier)
+            this.produitsPanier = JSON.parse(sessionStorage.produitsPanier)
+        console.log(this.produitsPanier)
         this.produitsPanier.push(item.dataset.itemId)
-        sessionStorage.produitsPanier = JSON.stringify(this.produitsPanier) //session storage ne prend que les strings
+        sessionStorage.produitsPanier = JSON.stringify(this.produitsPanier) 
     }
 
     verifier_inventaire_item = () =>{
