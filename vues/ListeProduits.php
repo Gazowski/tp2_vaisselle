21a74eead16f6d83443fe500cdf0507925152919
Gazowski <gael_comeau@hotmail.com>
2020-01-10 15:16:50 -0500

    ajout effet cubique sur les items

--- vues/ListeProduits.php
+++ vues/ListeProduits.php
@@ -1,20 +1,39 @@
 <div class="liste_produits" data-js-wrapper-liste>
 	<?php foreach($data as $produit) { ?>
-		<article class="tuile" 
-				data-js-item
-				data-item-id = <?= $produit['id'] ?>
-				data-item-nom = <?= str_replace(" ","_",$produit["nom"]) ?>>
-			<img src=<?= $produit['lienimage'] ?>
-				alt=<?= $produit['nom'] ?>>
-				<div>
-					<div>
-						<h3><?= $produit["nom"] ?></h3>
-						<h3 class="prix">prix : <?= $produit["prix"] . "$"?> </h3>
-						<p><span data-item-inventaire=<?= $produit['inventaire'] ?>></span> en stock</p>
-					</div>
-					<button class="btn-icon"><i class="fas fa-cart-plus"></i></button>
-				</div>
-		</article>
+		<div class="conteneur_cube">
+			<div class="cube">
+				<article class="tuile face" 
+						data-js-item
+						data-item-id = <?= $produit['id'] ?>
+						data-item-nom = <?= str_replace(" ","_",$produit["nom"]) ?>>
+					<img src=<?= $produit['lienimage'] ?>
+						alt=<?= $produit['nom'] ?>>
+						<div>
+							<div>
+								<h3><?= $produit["nom"] ?></h3>
+								<h3 class="prix">prix : <?= $produit["prix"] . "$"?> </h3>
+								<p><span data-item-inventaire=<?= $produit['inventaire'] ?>></span> en stock</p>
+							</div>
+							<button class="btn-icon"><i class="fas fa-cart-plus"></i></button>
+						</div>
+				</article>
+				<article class="tuile cote_droit" 
+						data-js-item
+						data-item-id = <?= $produit['id'] ?>
+						data-item-nom = <?= str_replace(" ","_",$produit["nom"]) ?>>
+					<img src=<?= $produit['lienimage'] ?>
+						alt=<?= $produit['nom'] ?>>
+						<div>
+							<div>
+								<h3><?= $produit["nom"] ?></h3>
+								<h3 class="prix">prix : <?= $produit["prix"] . "$"?> </h3>
+								<p><span data-item-inventaire=<?= $produit['inventaire'] ?>></span> en stock</p>
+							</div>
+							<button class="btn-icon"><i class="fas fa-cart-plus"></i></button>
+						</div>
+				</article>
+			</div>
+		</div>
 	<?php } ?>
 </div>
 
