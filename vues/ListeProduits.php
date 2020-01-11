01be7b7493cd0815e488aa11d6d50f84b22a77ff
Gazowski <gael_comeau@hotmail.com>
2020-01-11 18:06:26 -0500

    modification de la face cachée des cubes

--- vues/ListeProduits.php
+++ vues/ListeProduits.php
@@ -1,36 +1,23 @@
 <div class="liste_produits" data-js-wrapper-liste>
 	<?php foreach($data as $produit) { ?>
 		<div class="conteneur_cube">
-			<div class="cube">
-				<article class="tuile face" 
-						data-js-item
-						data-item-id = <?= $produit['id'] ?>
-						data-item-nom = <?= str_replace(" ","_",$produit["nom"]) ?>>
+			<div class="cube" 
+				data-js-item
+				data-item-id = <?= $produit['id'] ?>
+				data-item-nom = <?= str_replace(" ","_",$produit["nom"]) ?>>
+				<article class="tuile face">
 					<img src=<?= $produit['lienimage'] ?>
 						alt=<?= $produit['nom'] ?>>
 						<div>
-							<div>
-								<h3><?= $produit["nom"] ?></h3>
-								<h3 class="prix">prix : <?= $produit["prix"] . "$"?> </h3>
-								<p><span data-item-inventaire=<?= $produit['inventaire'] ?>></span> en stock</p>
-							</div>
-							<button class="btn-icon"><i class="fas fa-cart-plus"></i></button>
+							<h3><?= $produit["nom"] ?></h3>
+							<h3 class="prix">prix : <?= $produit["prix"] . "$"?> </h3>
+							<p><span data-item-inventaire=<?= $produit['inventaire'] ?>></span> en stock</p>
 						</div>
 				</article>
-				<article class="tuile cote_droit" 
-						data-js-item
-						data-item-id = <?= $produit['id'] ?>
-						data-item-nom = <?= str_replace(" ","_",$produit["nom"]) ?>>
+				<article class="tuile cote_droit" >
 					<img src=<?= $produit['lienimage'] ?>
 						alt=<?= $produit['nom'] ?>>
-						<div>
-							<div>
-								<h3><?= $produit["nom"] ?></h3>
-								<h3 class="prix">prix : <?= $produit["prix"] . "$"?> </h3>
-								<p><span data-item-inventaire=<?= $produit['inventaire'] ?>></span> en stock</p>
-							</div>
-							<button class="btn-icon"><i class="fas fa-cart-plus"></i></button>
-						</div>
+					<button class="btn-icon"><i class="fas fa-cart-plus"></i></button>
 				</article>
 			</div>
 		</div>
