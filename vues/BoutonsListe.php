36ab11e9a85ab2c1cecf0891b0536ba8ff270b0f
Gazowski <gael_comeau@hotmail.com>
2020-01-11 12:03:52 -0500

    css bouton pagination

--- vues/BoutonsListe.php
+++ vues/BoutonsListe.php
@@ -1,4 +1,5 @@
-
-<button data-js-btn-precedent disabled>Précédent</button>
-<button data-js-btn-suivant>Suivant</button>
+    <div class="btn-navigation">
+        <button data-js-btn-precedent disabled>Précédent</button>
+        <button data-js-btn-suivant>Suivant</button>
+    </div>
 </section>
