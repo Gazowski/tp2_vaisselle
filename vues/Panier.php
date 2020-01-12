4b0c310a3836dc39f2fbad0bd5cf909f53b1d92a
Gazowski <gael_comeau@hotmail.com>
2020-01-12 12:46:49 -0500

    css panier : mise en forme mobile

--- vues/Panier.php
+++ vues/Panier.php
@@ -1,23 +1,41 @@
 
-<div data-js-wrapper-liste>
+<div class="wrapper-list" data-js-wrapper-liste>
+    <div class="titre-col">
+        <div>Produit</div>
+        <div>Prix</div>
+        <div>Quantité</div>        
+    </div>
     <?php foreach($data as $id => $produit){
         $montant_total_item = $produit["prix"] * $produit["quantite"] ?>
-		<article class="tuile" 
+		<article class="tuile tuile-panier" 
 				data-js-item
                 data-item-id = <?= $produit['id'] ?>
                 data-item-prix = <?= $produit["prix"] ?>>
-            <p><?= $produit["nom"] . " - prix (unitaire) : " .  $produit["prix"] . "$"?></p>
-            <label for="quantite<?= $produit['id'] ?>">Quantité:</label>
-            <input type="number" 
-                    id="quantite<?= $produit['id'] ?>" 
-                    name="quantite<?= $produit['id'] ?>" 
-                    min="0" max="<?= $produit["inventaire"]?>"
-                    value="<?= $produit["quantite"]?>"
-                    data-js-choix-quantite>
-            quantité restante = <span data-item-inventaire = <?= $produit["inventaire"]?>><?= $produit["inventaire"]?></span> en stock
-            <p>prix total = <span data-js-montant-item><?= $montant_total_item ?></span>$</p>
+            <div>
+                <img src=<?= $produit['lienimage'] ?>
+                        alt=<?= $produit['nom'] ?>>
+            </div>
+            <div>
+                <h3><?= $produit["nom"] ?></h3>
+                <p><span data-item-inventaire = <?= $produit["inventaire"]?>><?= $produit["inventaire"]?></span> en stock</p>
+                <h3 class="prix"><?=  $produit["prix"]?>$</h3>
+            </div>
+            <div>
+                <label for="quantite<?= $produit['id'] ?>"></label>
+                <input type="number" 
+                        id="quantite<?= $produit['id'] ?>" 
+                        name="quantite<?= $produit['id'] ?>" 
+                        min="0" max="<?= $produit["inventaire"]?>"
+                        value="<?= $produit["quantite"]?>"
+                        data-js-choix-quantite>
+                <h3 class="prix">Total: <span data-js-montant-item><?= $montant_total_item ?></span>$</h3>
+            </div>
         </article>
     <?php } ?>
-    <article>Total Panier = <span data-js-montant-panier></span>$</article>
+    <article class="prix-total">
+        <div>
+            <h3>Total Panier : <span data-js-montant-panier></span>$</h3>
+            <button data-js-passer-commande>Passer la commande</button>
+        </div>
+    </article>
 </div>
-<button data-js-passer-commande>Passer la commande</button>
