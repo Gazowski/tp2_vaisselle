8572dcf6bd5b9182589c22e34b350c5d650f4b71
Comeau <e1895410@cmaisonneuve.qc.ca>
2020-01-22 12:11:19 -0500

    head.php : correction des balises meta

--- vues/common/Head.php
+++ vues/common/Head.php
@@ -4,16 +4,16 @@
     <head>
         <meta charset="UTF-8">
         <meta name="author" content='Gaël Comeau'>
-        <meta description='description' content='vaisselle , argenterie , couvert , verre, fourchette, couteau , assiette'>
-
-        <title>Boutique de Vaissellerie - TP2 (javascript)</title>
-
-        <!-- Ajustement de la taille du viewport -->
+        <meta name='description' content='boutique de vaisselerie artisanale.'>
+        <meta name='keywords' content='vaisselle , argenterie , couvert , verre, fourchette, couteau , assiette'>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>Boutique de Vaissellerie - TP2 (javascript)</title>
         
         <!-- Police de caractères -->
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,900&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Oswald:600|Raleway:300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Oswald:600&display=swap" rel="stylesheet">
         <!-- Feuilles de style -->
         <link rel="stylesheet" href="./styles/stylesheets/normalize.css">
         <link rel="stylesheet" href="./styles/sass/style.css">
